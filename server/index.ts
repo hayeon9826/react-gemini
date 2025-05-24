1e6daefac9e35ab24f8f5154ed1dc52986620891
김하연 <hayeonkim226@gmail.com>
2025-05-24 20:25:56 +0900

    fix: 불필요한 코드 삭제 (body parser)

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser";
 import chatRouter from "./routes/chat";
 import geminiStreamRouter from "./routes/geminiStream";
 import path from "path";
@@ -8,8 +7,6 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use("/api", geminiStreamRouter);
-//  JSON 파싱이 필요한 라우터는 body-parser 이후에 마운트
-app.use(bodyParser.json());
 app.use("/api", chatRouter);
 
 // GET 테스트
